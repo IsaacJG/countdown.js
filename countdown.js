b648013b71d0673f99d7ea0763dbdecb0dbcf5a3
Isaac Grant <waterwolf2.0@gmail.com>
2013-06-24 21:03:52 -0600

    add timer function if no query is present

--- countdown.js
+++ countdown.js
@@ -29,20 +29,24 @@
         return final;
     }
     
-    function getTimeDifference(time) {
+    function getTimeDifference(time, origin) {
         var seconds = (time[0]*3600) + (time[1]*60) + time[2];
-        var d = new Date();
-        var now = (d.getHours()*3600) + (d.getMinutes()*60) + d.getSeconds();
-        var difference = seconds - now;
-        if (difference > 0) {
-            var minutes = Math.floor(difference / 60);
-            difference -= (minutes * 60);
-            var hours = Math.floor(minutes / 60);
-            minutes -= (hours * 60);
-            return [hours, minutes, difference];
+        if (typeof(origin) === undefined) {
+            var d = new Date();
+            originSeconds = (d.getHours()*3600) + (d.getMinutes()*60) + d.getSeconds();
         } else {
-            return false;
+            originSeconds = (origin[0]*3600) + (origin[1]*60) + origin[2];
         }
+        var difference = seconds - originSeconds;
+            if (difference > 0) {
+                var minutes = Math.floor(difference / 60);
+                difference -= (minutes * 60);
+                var hours = Math.floor(minutes / 60);
+                minutes -= (hours * 60);
+                return [hours, minutes, difference];
+            } else {
+                return false;
+            }
     }
     
     function tick() {
@@ -56,8 +60,25 @@
             	flash(true);
             }
         } else {
-            
+            $('#timer').text('00:00:00');
+            timerTick();
         }
+    }
+
+    function timerTick() {
+        var d = new Date();
+        var begin = [d.getHours(), d.getMinutes(), d.getSeconds()];
+        function update() {
+            var now = new Date();
+            var diff = getTimeDifference([now.getHours(), now.getMinutes(), now.getSeconds()], begin);
+            if (diff) {
+                $('#timer').text(synthesizeTime(diff));
+            } else {
+                console.log(diff);
+            }
+            setTimeout(update, 1);
+        }
+        update();
     }
 
     function flash(black) {
